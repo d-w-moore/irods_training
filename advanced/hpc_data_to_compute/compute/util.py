0b40fbf418f611da23f425ed64d1c53eb17610e3
d-w-moore <dmoore@renci.org>
2019-06-20 11:07:00 +0000

    cut out excess

--- advanced/hpc_data_to_compute/compute/util.py
+++ advanced/hpc_data_to_compute/compute/util.py
@@ -20,17 +20,22 @@ def get_job_param_as_string (rule_args, callback, rei):
     if retval is None: retval = str(param_value)
     rule_args[1] = retval
 
-def compute_host_from_rescName (rescName):
-  pass
+#--------------------------------------------------------------
+#def compute_rescName__as_rule (args,callback,rei):
+#  kw = {'callback':callback, 'rei':rei}
+#  rescName = compute_rescName(*args[:1], **kw)
+#  # -- return value : resource's literal name 
+#  if len(args) > 1: args[1] =  rescName
+#def compute_host_from_rescName (rescName):
+#  pass
+#def pyParseRoleSpec (rule_args,callback,rei):  #-- called from iRODS rule language
+#    compute_resc_spec = rule_args[0]
+#    rule_args[1:3]= map( lambda x:x.strip() , 
+#                         (compute_resc_spec.split('=')+['']) [:2] )
 
 chksum_opt = { k:'1' for k in ( REG_CHKSUM_KW,VERIFY_CHKSUM_KW,CHKSUM_KW ) }
 
-def pyParseRoleSpec (rule_args,callback,rei):  #-- called from iRODS rule language
-    compute_resc_spec = rule_args[0]
-    rule_args[1:3]= map( lambda x:x.strip() , 
-                         (compute_resc_spec.split('=')+['']) [:2] )
-
-def compute_rescName__as_rule ( compute_resc_spec, session = None, **kw ):
+def compute_rescName ( compute_resc_spec, session = None, **kw ):
 
  #log_method = kw.get ( 'log_method', lambda x: None ):
   sess = kw.get ('session')
@@ -50,8 +55,8 @@ def compute_rescName__as_rule ( compute_resc_spec, session = None, **kw ):
   else:
     resc = compute_resc_spec \
            if compute_resc_spec \
-           else "demoResc"		#__ the default for now
-					#    (should be configurable, however)
+           else "demoResc"
+
   if (role_key and role_value):
     if sess:
       q = sess.query( Resource.name , ResourceMeta.name , ResourceMeta.value )
@@ -63,9 +68,3 @@ def compute_rescName__as_rule ( compute_resc_spec, session = None, **kw ):
         pass
   return str( resc )
 
-#--------------------------------------------------------------
-def compute_rescName__as_rule (args,callback,rei):
-  kw = {'callback':callback, 'rei':rei}
-  rescName = compute_rescName(*args[:1], **kw)
-  # -- return value : resource's literal name 
-  if len(args) > 1: args[1] =  rescName
--- advanced/hpc_data_to_compute/spawn_remote_slurm_jobs.r
+++ advanced/hpc_data_to_compute/spawn_remote_slurm_jobs.r
@@ -1,44 +1,48 @@
-run_tasks
+run_remote_image_thumbnail_tasks
 {
     get_host_and_resource_name_by_role(*host, *resc_name, *role_key, *role_val) 
-    writeLine ("stdout", "host=[*host] resc=[*resc_name]")
+
     if ("*host" == "" ) {
         writeLine ("stdout", "Host for job launch was not found.")
     } else {
-        writeLine("stdout","host = *host")
-    
 
-    remote(*host, "") {
-      *input_file_path = "";get_job_param_as_string("input_path",*input_file_path)
-      writeLine("stdout", "hello input ---> *input_file_path")
-      foreach (*x in select DATA_PATH,DATA_NAME where 
-	  COLL_NAME = '*input_file_path' and
-	  RESC_NAME = '*resc_name' and
-	  DATA_NAME like '%.jpg' || like '%.gif' || like '%.tif')
-      {
-	  *input_file_name = *x.DATA_NAME
-	  *src_phy_path = *x.DATA_PATH
-      }
-      writeLine("stdout", "image phy path => *src_phy_path")
-      writeLine("stdout", "image data name => *input_file_name")
-      *el=split(*input_file_name,".")
-      *input_file_stub_name = elem(*el,0)
-      *input_file_ext = elem(*el,1)
+        remote(*host, "") {
 
-      *thumbnail_sizes = ""
-      get_job_param_as_string("thumbnail_sizes",*thumbnail_sizes)
-  #     *dst_dir = ""; get_job_param_as_string( "phys_dir_for_output", *dst_dir)
-  ####  foreach (*size in split (*thumbnail_sizes, ",")) {
-  ##      writeLine ("serverLog", "job for thumbnail size : [*size] - running on host [ *host ]")
-  ##      *dst_phy_path = *dst_dir" ++ "*input_file_stub_name" ++ "_thumbnail_" ++ "*size" ++ "*input_file_ext"
-  ##      writeLine ("serverLog", " dst_phy_path --> [*dst_phy_path]")
-  #       writeLine("serverLog"," - thumbsize [ *size ]; convert ( *src_phy_path , *dst_phy_path )")
-  #       *cmd_opts="/var/lib/irods/msiExecCmd_bin/convert.SLURM -thumbnail *size *src_phy_path *dst_phy_path"
-  #       msiExecCmd("submit_thumbnail_job.sh","*cmd_opts","null","null","null",*OUT)
-  ####  } #foreach
-    } #remote
-    } #if-else
-} # end rule
+            *input_file_path = "";get_job_param_as_string("input_path",*input_file_path)
+
+            *src_phy_path = ""
+            foreach (*x in select DATA_PATH,DATA_NAME where 
+                COLL_NAME = '*input_file_path' and
+                DATA_RESC_NAME = '*resc_name' and
+                DATA_NAME like '%.jpg' || like '%.gif' || like '%.tif')
+            {
+                *input_file_name = *x.DATA_NAME
+                *src_phy_path = *x.DATA_PATH
+            }
+
+            if (*src_phy_path == "") {
+                writeLine("stdout","no images for input")
+            }
+            else {
+                *el=split(*input_file_name,".")
+                *input_file_stub_name = elem(*el,0)
+                *input_file_ext = elem(*el,1)
+
+                *thumbnail_sizes = ""
+                get_job_param_as_string("thumbnail_sizes",*thumbnail_sizes)
+                *dst_dir = ""; get_job_param_as_string( "phys_dir_for_output", *dst_dir)
+
+                foreach (*size in split (*thumbnail_sizes, ",")) {
+                    writeLine ("stdout" , "launching remote task for thumbnail size: *size")
+                    *dst_phy_path = "*dst_dir/" ++ "*input_file_stub_name" ++ "_thumbnail_*size.*input_file_ext"
+                    *cmd_opts="/var/lib/irods/msiExecCmd_bin/convert.SLURM -thumbnail *size *src_phy_path *dst_phy_path"
+                    msiExecCmd("submit_thumbnail_job.sh","*cmd_opts","null","null","null",*OUT)
+                } 
+            }
+        } 
+    } 
+
+} 
 
 get_host_and_resource_name_by_role (*host, *resc_name, *attr, *value) 
 {
