adfb34a143fefe5e0ac707f2b4fa03ae04a47e14
d-w-moore <dmoore@renci.org>
2019-06-20 11:34:01 +0000

    example resc-name lookup via iquest

--- advanced/hpc_data_to_compute/rescName_from_compute_role_kvpair.sh
+++ advanced/hpc_data_to_compute/rescName_from_compute_role_kvpair.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-RESC_NAME=$( iquest '%s' "select RESC_NAME where  META_RESC_ATTR_NAME = 'COMPUTE_RESOURCE_ROLE' and META_RESC_ATTR_VALUE = '$1' ")
+if [ $# -lt 2 ] ; then
+  echo >&2 "iRODS Data To Compute training -- resource query utility"
+  echo >&2 "  (Look up resource name in ICAT by compute-related role key/value)"
+  echo >&2 "example uses: $0  'COMPUTE_RESOURCE_ROLE'  'IMAGE_PROCESSING'"
+  echo >&2 "              $0  'COMPUTE_RESOURCE_ROLE'  'LONG_TERM_STORAGE'"
+  exit 1
+fi
+RESC_NAME=$( iquest '%s' "select RESC_NAME where  META_RESC_ATTR_NAME = '$1' and META_RESC_ATTR_VALUE = '$2' ")
 echo "$RESC_NAME"
