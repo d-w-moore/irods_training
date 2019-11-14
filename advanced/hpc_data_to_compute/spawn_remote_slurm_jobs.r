19ae5617d7485ce65c96b2936092c4170b67e283
d-w-moore <dmoore@renci.org>
2019-11-14 07:47:47 -0500

    reformat spawn jobs rulefile

--- advanced/hpc_data_to_compute/spawn_remote_slurm_jobs.r
+++ advanced/hpc_data_to_compute/spawn_remote_slurm_jobs.r
@@ -1,26 +1,27 @@
 main_rule
 {
-  *resc_name = ""
-  *host = host_for_resource_role ( *key, *val )
-  writeLine ("stdout", "host=[*host]")
-  if (*host == "") { writeLine("stderr", 
-                               "null host - please define COMPUTE_RESOURCE_ROLE metadata")
-  }
-  else {
-        remote(*host, "") { msiExecCmd("submit_thumbnail_jobs.py","--input '*default_input'",
-                                       "null","null","null", *OUT) }
-  }
+    *host = host_for_resource_role ( *key, *val )
+    writeLine ("stdout", "host=[*host]")
+    if (*host == "") {
+        writeLine("stderr", "null host - please define COMPUTE_RESOURCE_ROLE metadata")
+    }
+    else {
+        remote(*host, "") {
+            msiExecCmd("submit_thumbnail_jobs.py","--input '*default_input'",
+                       "null","null","null", *OUT)
+        }
+    }
 }
 
 host_for_resource_role (*key, *value)
 {
-  *host = ""
-  foreach (*h in select RESC_LOC where META_RESC_ATTR_NAME = '*key' 
-                                   and META_RESC_ATTR_VALUE = '*val')
-  {
-    *host = *h.RESC_LOC
-  }
-  *host
+    *host = ""
+    foreach (*h in select RESC_LOC where META_RESC_ATTR_NAME = '*key'
+                                     and META_RESC_ATTR_VALUE = '*val')
+    {
+        *host = *h.RESC_LOC
+    }
+    *host
 }
 
 INPUT *default_input="/$rodsZoneClient/home/$userNameClient/stickers.jpg", *key="COMPUTE_RESOURCE_ROLE", *val="IMAGE_PROCESSING"
